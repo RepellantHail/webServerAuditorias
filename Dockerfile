22c9e4356e3457675fefd3155439d86d7d7ac761
RepellantHail <a20310068@ceti.mx>
2024-06-11 17:00:06 -0600

    server config and webapp setup. Not migrating

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3
 
 #Set enviroment variables
 ENV PYTHONBUFFERED=1
-ENV PORT 8080
+ENV PORT 3000
 
 #Working dir
 WORKDIR /app
@@ -12,6 +12,8 @@ COPY requirements.txt ./
 #Install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 RUN pip install --upgrade pip
+RUN pip install python-dotenv
+
 
 #Copy Django Project
 COPY . /app/
--- server/settings/local.py
+++ server/settings/local.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    '../webserver_app'
 ]
 
 MIDDLEWARE = [
