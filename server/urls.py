fa6e5814d113eac9b2c0c852e27dc59131b26cd7
RepellantHail <a20310068@ceti.mx>
2024-06-15 16:48:51 -0600

    docker compose running & web app working, not able to have a stationary db. Created a volume on every reset

--- server/urls.py
+++ server/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('server.webserver_app.urls')),
+    path('', include('server.webserver_app.urls')), #Include app urls
 ]
